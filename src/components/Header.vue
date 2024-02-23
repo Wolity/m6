e7753f9023849e1a465e97fdabdc587be61bd390
Wolity <astapovsasa6@gmail.com>
2024-02-23 19:10:25 +0200

    main

---
+++ src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+    <header>
+        голова сайта
+    </header>
+</template>
---
+++ src/views/Main.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <h1>Welcome</h1>
+  </section>
+</template>
+<style scoped>
+h1{
+    margin-top: 250px;
+    font-size: 100px;
+    color: white;
+    text-align: center;
+}
+</style>
