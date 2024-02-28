05a19ac71328c9bc1b2d9281226f01a88f4df439
Wolity <astapovsasa6@gmail.com>
2024-02-28 20:42:50 +0200

    third

--- src/components/GameCard.vue
+++ src/components/GameCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <img
-      scr="https://upload.wikimedia.org/wikipedia/ru/thumb/3/33/Hogwarts_Legacy_cover.jpg/800px-Hogwarts_Legacy_cover.jpg"
+      src="https://image.api.playstation.com/vulcan/ap/rnd/202208/0921/3XopdGAJGRy3xNQKnQDvaCRs.png"
     />
     <h1>Title</h1>
     <p>descreption</p>
@@ -13,6 +13,42 @@
       <i class="fas fa-desktop"></i>
       <h4>Windows</h4>
     </nav>
-    <router-link to="/">Подробднее</router-link>
+    <router-link to="/">Подробнее</router-link>
   </div>
 </template>
+<style scoped>
+div {
+  width: 100%;
+  padding: 10px;
+  background: #112c7b;
+  color: rgb(255, 255, 255);
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+}
+h1 {
+  font-size: 32px;
+  margin-top: 20px;
+}
+p {
+  font-size: 20px;
+  margin-top: 20px;
+}
+nav {
+  display: flex;
+  margin-top: 20px;
+}
+i,
+h4 {
+  font-size: 20px;
+  margin-right: 10px;
+}
+a {
+  background: rgb(0, 0, 0);
+  font-size: 24px;
+  padding: 10px 10px;
+  margin-top: 20px;
+  margin-left: auto;
+  border-radius: 10px; 
+}
+</style>
--- src/views/Fantasy.vue
+++ src/views/Fantasy.vue
@@ -1,3 +1,29 @@
+<script setup>
+import GameCard from "../components/GameCard.vue";
+</script>
+
 <template>
-  <section>Страница с Fantasy</section>
+  <section>
+    <h1 class="title">Fantasy</h1>
+    <div class="card_block">
+      <GameCard />
+      <GameCard />
+      <GameCard />
+    </div>
+  </section>
 </template>
+
+<style>
+.title {
+  font-size: 40px;
+  text-align: center;
+  margin: 20px 0;
+  color: rgb(255, 255, 255);
+}
+.card_block {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 50px;
+  margin:0 50px;
+}
+</style>
